d294f9180dd811b507afb4527d717fa09e05b48d
moviesenderggbot <157599470+moviesenderggbot@users.noreply.github.com>
2024-03-22 14:01:45 +0530

    Update info.py

--- info.py
+++ info.py
@@ -51,7 +51,7 @@ NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", False)) # True if you want n
 
 # MongoDB information
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://fakave7698:vUjf8bPQGPanrLH7@cluster0.crtwuc0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-DATABASE_NAME = environ.get('DATABASE_NAME', "damo74637")
+DATABASE_NAME = environ.get('DATABASE_NAME', "fakave7698")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Verify
